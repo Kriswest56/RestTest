15251c6d0ace57cf94c4ff5e67933c4ab7ff4aa2
Kriswest56 <broncosfreak56@gmail.com>
2018-04-07 16:01:29 -0600

    Initial commit

---
+++ src/main/java/com/kris/resources/MyResource.java
@@ -0,0 +1,35 @@
+package com.kris.resources;
+
+
+import com.google.gson.JsonObject;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+public class MyResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt(){
+        return "Resource Home";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{name}")
+    public String getIt(@PathParam("name") String name){
+
+        String param = name;
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("Hello", param);
+
+        return jsonObject.toString();
+
+    }
+
+}
